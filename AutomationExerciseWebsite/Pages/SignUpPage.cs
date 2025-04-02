05402c4b7ed960aa3a69e7a54b6af4947a36f76b
Buyukbek <greatboy_94@mail.ru>
2025-04-03 00:41:54 +0500

    Register User test case done

--- AutomationExerciseWebsite/Pages/SignUpPage.cs
+++ AutomationExerciseWebsite/Pages/SignUpPage.cs
@@ -15,7 +15,17 @@ public class SignUpPage : BasePage
     private readonly By birthYears = By.Id("years");
     private readonly By newsLetter = By.Id("newsletter");
     private readonly By receiveOffers = By.XPath("//input[@name='optin']");
-    private readonly By address1 = By.Id("address1");
+    private readonly By firstNameInput = By.Id("first_name");
+    private readonly By lastNameInput = By.Id("last_name");
+    private readonly By companyInput = By.Id("company");
+    private readonly By address1input = By.Id("address1");
+    private readonly By address2Input = By.Id("address2");
+    private readonly By chooseCountry = By.Id("country");
+    private readonly By stateInput = By.Id("state");
+    private readonly By cityInput = By.Id("city");
+    private readonly By zipcodeInput = By.Id("zipcode");
+    private readonly By mobileNumberInput = By.Id("mobile_number");
+    private readonly By createAccountButton = By.XPath("//button[@data-qa='create-account']");
     
     
     public SignUpPage(IWebDriver driver) : base(driver)
@@ -27,17 +37,19 @@ public class SignUpPage : BasePage
         return IsElementDisplayed(loginForm);
     }
 
-    public SignUpPage CreateAccountButton(string password)
+    public SignUpPage CreateAccountWithCredentials(string password, string firstName, string lastName, string company, string address1, string address2, string state, string city, string zipcode, string mobileNumber)
     {
-        ClickToMr();
+        ChooseGender();
         EnterPassword(password);
         SelectDateOfBirth();
         ClickToNewsLetterAndReceiveOffers();
+        FillAddressForm(firstName, lastName, company, address1, address2, state, city, zipcode, mobileNumber);
+        ClickToCreateAccountButton();
 
         return new SignUpPage(driver);
     }
 
-    public void ClickToMr()
+    public void ChooseGender()
     {
         Click(genderMr);
         bool isSelected = driver.FindElement(genderMr).Selected;
@@ -73,5 +85,25 @@ public class SignUpPage : BasePage
             throw new InvalidOperationException("The News Letter and Receive Offer is not checked. ");
         }
         
+    }
+
+    public void FillAddressForm(string firstName, string lastName, string company, string address1, string address2, string state, string city, string zipcode, string mobileNumber)
+    {
+        SendKeys(firstNameInput, firstName);
+        SendKeys(lastNameInput, lastName);
+        SendKeys(companyInput, company);
+        SendKeys(address1input, address1);
+        SendKeys(address2Input, address2);
+        SelectDropDownByValue(chooseCountry, "United States");
+        SendKeys(stateInput, state);
+        SendKeys(cityInput, city);
+        SendKeys(zipcodeInput, zipcode);
+        SendKeys(mobileNumberInput, mobileNumber);
+        ScrollToElement(stateInput);
+    }
+
+    public void ClickToCreateAccountButton()
+    {
+        Click(createAccountButton);
     }
 }
